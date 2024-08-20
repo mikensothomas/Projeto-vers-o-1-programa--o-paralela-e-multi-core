d1a2a11db3fb2c8df4e026ece71dbcb6bcd42a6c
mikensothomas <thomasmikenson40@gmail.com>
2024-08-20 19:08:20 -0300

    Commit

--- client.py
+++ client.py
@@ -13,37 +13,10 @@ def cliente(host = 'localhost', port = 5000):
         print("Mensagem enviado com sucesso")
         sock.sendall(message.encode('utf-8'))
 
+        data = sock.recv(1024)
+        print("Mensagem do servidor: ", data.decode('utf-8'))
+
     finally:
         sock.close()
 
 cliente()
-
-# def client(host = 'localhost', port=8082): 
-#     # Create a TCP/IP socket 
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-#     # Connect the socket to the server 
-#     server_address = (host, port) 
-#     print ("Connecting to %s port %s" % server_address) 
-#     sock.connect(server_address) 
-#     # Send data 
-#     try: 
-#         # Send data 
-#         message = "Test message. This will be echoed" 
-#         print ("Sending %s" % message) 
-#         sock.sendall(message.encode('utf-8')) 
-#         # Look for the response 
-#         amount_received = 0 
-#         amount_expected = len(message) 
-#         while amount_received < amount_expected: 
-#             data = sock.recv(16) 
-#             amount_received += len(data) 
-#             print ("Received: %s" % data) 
-#     except socket.error as e: 
-#         print ("Socket error: %s" %str(e)) 
-#     except Exception as e: 
-#         print ("Other exception: %s" %str(e)) 
-#     finally: 
-#         print ("Closing connection to the server") 
-#         sock.close() 
-
-# client()
