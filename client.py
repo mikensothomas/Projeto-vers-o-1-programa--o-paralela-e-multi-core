14abd1b1d7b2a81dd81f6f825d56801f7a6fb89a
mikensothomas <thomasmikenson40@gmail.com>
2024-08-20 20:10:01 -0300

    Commit

--- client.py
+++ client.py
@@ -1,21 +1,30 @@
-
 import socket
 
-def cliente(host = 'localhost', port = 5000):
+def cliente(host='localhost', port=5000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (host, port)
-
-    print("Conectado com o servidor no %s na porta %s" % server_address)
     sock.connect(server_address)
 
     try:
-        message = input("Digite sua mensagem: ")
-        sock.sendall(message.encode('utf-8'))
-        print("Mensagem enviado com sucesso")
-
         data = sock.recv(1024)
-        print("Mensagem do servidor: ", data.decode('utf-8'))
+        print(data.decode('utf-8'))
 
+        client_name = input("Digite seu nome: ")
+        sock.sendall(client_name.encode('utf-8'))
+        print("Conectado com o servidor no %s na porta %s" % server_address)
+
+        while True:
+            menu = sock.recv(1024).decode('utf-8')
+            print(menu)
+
+            choice = input("Escolha uma opção: ")
+            sock.sendall(choice.encode('utf-8'))
+
+            data = sock.recv(1024)
+            if data:
+                print("Mensagem do servidor:", data.decode('utf-8'))
+            else:
+                break
     finally:
         sock.close()
 
--- server.py
+++ server.py
@@ -1,23 +1,27 @@
-
 import socket
 
-def servidor(host = 'localhost', port = 5000):
+def servidor(host='localhost', port=5000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (host, port)
     sock.bind(server_address)
     sock.listen()
 
-    print("O servidor está conectado no %s e na porta %s" % server_address)
     print("Esperando a conexão com clientes...")
     client, address = sock.accept()
-    print("Conectado com um cliente", address)
 
     try:
+        client.sendall("Por favor, insira seu nome no campo embaixo ".encode('utf-8'))
+        client_name = client.recv(1024).decode('utf-8')
+        print(f"O servidor está conectado no {server_address[0]} na porta {server_address[1]} com o cliente {client_name}")
+
         while True:
+            menu_message = "Menu:\n1. Opção 1\n2. Opção 2\nDigite sua escolha: "
+            client.sendall(menu_message.encode('utf-8'))
+
             data = client.recv(1024)
 
             if data:
-                print("Mensagem recebida do cliente: ", data.decode('utf-8'))
+                print(f"Mensagem recebida de {client_name}: ", data.decode('utf-8'))
                 message = input("Digite sua mensagem: ")
                 client.sendall(message.encode('utf-8'))
                 print("Mensagem enviada com sucesso")
